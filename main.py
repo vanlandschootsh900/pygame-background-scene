5ab6b22506f45b50348b5c2fc8c6e24ad4c1bf16
vanlandschootsh900 <vanlandschootsh900@northwested.org>
2025-03-07 10:37:42 -0500

    s

--- main.py
+++ main.py
@@ -28,25 +28,35 @@ def draw_text(screen, text, font, text_color, x,y):
     img = font.render(text, True, text_color)
     screen.blit(img, (x,y))
 
-def draw_cloud(screen, pos, pos1, pos2, pos3):
-    pygame.draw.circle(screen, config.WHITE, pos, 50)
+
+def draw_cloud(screen, x,y):
+    pygame.draw.circle(screen, config.WHITE, (x,y), 50)
 
     
-    pygame.draw.circle(screen, config.WHITE, pos1, 50)
-    pygame.draw.circle(screen, config.WHITE, pos2, 50)
-    pygame.draw.circle(screen, config.WHITE, pos3, 50)
+    pygame.draw.circle(screen, config.WHITE, (50+x,y), 50)
+    pygame.draw.circle(screen, config.WHITE, ((-50)+x,y), 50)
+    pygame.draw.circle(screen, config.WHITE, (x,(-50)+y), 50)
+
 
 def sun(screen, pos):
     pygame.draw.circle(screen, config.YELLOW, pos, 75)
 
 
-def guy(screen,start, end,right,left, arms,arme):
-    pygame.draw.line(screen, config.BLACK, start,end,5)
+#def guy(screen,start, end,right,left, arms,arme):
+def guy(screen, start_x, start_y, end_x, end_y):
+    pygame.draw.line(screen, config.BLACK, (start_x,start_y),(end_x,end_y),5)
     
-    pygame.draw.line(screen,config.BLACK,end, left,5)
-    pygame.draw.line(screen,config.BLACK,end,right,5)
-    pygame.draw.line(screen,config.BLACK,arms,arme,5)
-    pygame.draw.circle(screen, config.BLACK, start ,15)
+
+    pygame.draw.line(screen,config.BLACK, (end_x,end_y), (80+start_x,50+start_y), 5)
+    pygame.draw.line(screen,config.BLACK, (end_x,end_y), (45+start_x,80+start_y), 5)
+    pygame.draw.line(screen,config.BLACK,((-10)+start_x,50+start_y) , (30+end_x,(-50)+end_y) ,5)
+    pygame.draw.circle(screen, config.BLACK,(start_x,start_y) ,15)
+
+
+    # pygame.draw.line(screen,config.BLACK,end, left,5)
+    # pygame.draw.line(screen,config.BLACK,end,right,5)
+    # pygame.draw.line(screen,config.BLACK,arms,arme,5)
+    # pygame.draw.circle(screen, config.BLACK, start ,15)
 
 
 
@@ -68,12 +78,14 @@ def main():
         draw_text(screen,'THE SKY', text_font, config.PURPLE,300,100)
         draw_text(screen,'Guy Falling',text_font,config.PURPLE,150, 400)
         
-
-        draw_cloud(screen, (200,300), (250,300),(150,300),(200,250))
-        draw_cloud(screen, (500,400),(450,400),(550,400),(500,350))
-        draw_cloud(screen, (600,130),(550,130),(650,130),(600,80))
+        draw_cloud(screen, 200,300)
+        draw_cloud(screen, 500,400)
+        draw_cloud(screen, 600,130)
         sun(screen, (100,100))
-        guy(screen,(200,500),(250,550),(245,580),(280,550),(190,550),(240,500))
+
+        guy(screen,200,500,250,550)
+        #guy(screen,(200,500),(250,550),(245,580),(280,550),(190,550),(240,500))
+
         draw_text(screen,'sun',text_font1,config.PURPLE,80,75)
         draw_text(screen,'CLOUD',text_font1,config.PURPLE,460,380)
         
